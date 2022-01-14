db2175816c1e2e1eb5be37b6c6a1721679fb2d53
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2022-01-14 12:36:06 -0300

    primeira etapa finalizada

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const ano = document.querySelector('#ano')
+const mes = document.querySelector('#mes')
+const dia = document.querySelector('#dia')
+const tipo = document.querySelector('#tipo')
+const descricao = document.querySelector('#descricao')
+const valor = document.querySelector('#valor')
+
 class Despesa {
     constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano
@@ -8,7 +15,7 @@ class Despesa {
         this.valor = valor
     }
 
-    validarDados() {
+    validarDados() { //verifica se todos os campos estão preenchidos
         for(let i in this) {
             if(this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false
@@ -39,21 +46,32 @@ class Bd {
             if(despesa === null) {
                 continue
             }
+            despesa.id = i //adiciona um id à cada despesa recuperada
             despesas.push(despesa)
         }
         return despesas
+    }
+
+    filtrarDespesas(despesa) {
+        let despesasFiltradas = Array()
+        despesasFiltradas = this.recuperarRegistros()
+
+        for(let i in despesa) {
+            if(despesa[i] != ''){
+                despesasFiltradas = despesasFiltradas.filter(d => d[i] == despesa[i])
+            }
+        }
+
+        return despesasFiltradas
+    }
+
+    removerRegistro(id) {
+        localStorage.removeItem(id)
     }
 }
 let bd = new Bd()
 
 function cadastrarDespesa() {
-    const ano = document.querySelector('#ano')
-    const mes = document.querySelector('#mes')
-    const dia = document.querySelector('#dia')
-    const tipo = document.querySelector('#tipo')
-    const descricao = document.querySelector('#descricao')
-    const valor = document.querySelector('#valor')
-
     let despesa = new Despesa(ano.value, mes.value, dia.value, 
         tipo.value, descricao.value, valor.value)
 
@@ -66,13 +84,9 @@ function cadastrarDespesa() {
     }
 }
 
-function limparCampos() {
-    ano.value = '' 
-    mes.value = '' 
-    dia.value = ''
-    tipo.value = ''
-    descricao.value = '' 
-    valor.value = ''
+function limparCampos() { 
+    let campos = document.querySelectorAll('#formulario select, #formulario input')
+    for(i in campos) campos[i].value = ""
 }
 
 function mostrarModal(msg) {
@@ -101,10 +115,14 @@ function carregarDespesas() {
     let despesas = Array()
     despesas = bd.recuperarRegistros()
 
-    //inserir dados na tabela:
-    const tabela = document.querySelector('#corpo-tabela')
+    mostrarElementosNaTabela(despesas)
+} 
 
-    despesas.forEach((d) => {
+function mostrarElementosNaTabela(array) {
+    const tabela = document.querySelector('#corpo-tabela')
+    tabela.innerHTML = ''
+
+    array.forEach((d) => {
         let linha = tabela.insertRow()
         let dia = d.dia < 10 ? `0${d.dia}` : d.dia
         let mes = d.mes < 10 ? `0${d.mes}` : d.mes
@@ -113,7 +131,17 @@ function carregarDespesas() {
         linha.insertCell(1).innerHTML = verificarTipo(d.tipo)
         linha.insertCell(2).innerHTML = d.descricao
         linha.insertCell(3).innerHTML = d.valor
-
+        
+        //inserir botao de exclusão:
+        let btn = document.createElement('button')
+        btn.className = 'btn btn-danger btn-sm'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id-despesa-${d.id}`
+        btn.onclick = () => {
+            bd.removerRegistro(d.id)
+            linha.remove()
+        }
+        linha.insertCell(4).append(btn)
     })
 
     function verificarTipo(tipo) {
@@ -125,4 +153,13 @@ function carregarDespesas() {
             case '5': return 'Transporte';
         }
     }
-} 
+}
+
+function pesquisarDespesa() {
+    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+
+    let despesaFiltrada = bd.filtrarDespesas(despesa)
+
+    mostrarElementosNaTabela(despesaFiltrada)
+    limparCampos()
+}
